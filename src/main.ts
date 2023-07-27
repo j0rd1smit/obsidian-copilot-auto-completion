e15cb320ecde5d653a8ba6fd4ccdb0bcbbe1f6a2
Jordi Smit <j0rd1smit@users.noreply.github.com>
2023-07-27 10:18:59 +0200

    Refactor: made failed settings load less agressive

--- src/main.ts
+++ src/main.ts
@@ -1,12 +1,12 @@
 import {Editor, MarkdownView, Notice, Plugin} from "obsidian";
 
-import { DEFAULT_SETTINGS, Settings, SettingTab } from "./settings/settings";
+import {DEFAULT_SETTINGS, Settings, SettingTab} from "./settings/settings";
 import EventListener from "./event_listener";
 import StatusBar from "./status_bar";
 import DocumentChangesListener from "./render_plugin/document_changes_listener";
-import { EditorView } from "@codemirror/view";
+import {EditorView} from "@codemirror/view";
 import RenderSuggestionPlugin from "./render_plugin/render_surgestion_plugin";
-import { InlineSuggestionState } from "./render_plugin/states";
+import {InlineSuggestionState} from "./render_plugin/states";
 import CompletionKeyWatcher from "./render_plugin/completion_key_watcher";
 import {hasSameAttributes} from "./settings/utils";
 
@@ -137,26 +137,40 @@ export default class CopilotPlugin extends Plugin {
     }
 
     private async saveSettings(settings: Settings): Promise<void> {
-        const data = { settings: settings };
+        const data = {settings: settings};
         await this.saveData(data);
     }
 
     private async loadSettings(): Promise<Settings> {
         const data = Object.assign(
             {},
-            { settings: DEFAULT_SETTINGS },
+            {settings: DEFAULT_SETTINGS},
             await this.loadData()
         );
         const settings = data.settings;
         if (!hasSameAttributes(settings, DEFAULT_SETTINGS)) {
             new Notice("Copilot: Could not load settings, reverting to default settings");
-            return DEFAULT_SETTINGS;
+            const azureOAIApiSettings = {
+                ...settings.azureOAIApiSettings,
+            };
+            const openAIApiSettings = {
+                ...DEFAULT_SETTINGS.openAIApiSettings,
+                key: settings.openAIApiSettings.key,
+            };
+
+            const defaultSettings: Settings = {
+                ...DEFAULT_SETTINGS,
+                apiProvider: settings.apiProvider,
+                azureOAIApiSettings,
+                openAIApiSettings,
+                advancedMode: settings.advancedMode,
+            };
+            return defaultSettings;
         }
 
         return settings;
     }
 
     onunload() {
-        // TODO preform cleanup
     }
 }
