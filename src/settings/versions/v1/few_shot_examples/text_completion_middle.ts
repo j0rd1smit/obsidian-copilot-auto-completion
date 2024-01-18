59e245f8287ce8a0ee4f811e055740e5a67c400e
Jordi Smit <j0rd1smit@users.noreply.github.com>
2024-01-18 16:47:41 +0100

    refactor: improved fill in the middle example

--- src/settings/versions/v1/few_shot_examples/text_completion_middle.ts
+++ src/settings/versions/v1/few_shot_examples/text_completion_middle.ts
@@ -4,11 +4,11 @@ import Context from "../../../../context_detection";
 const example: FewShotExample = {
     context: Context.Text,
     input: `# Digitizing sound waves 
-Typical sound waves are complex and consist of multiple waves, each with <mask/>
-When we digitize a sound, we sample the amplitude, the difference compared to the base level, at fixed intervals. This gives a sequence of digital values that can be used to approximate the original sound wave by recreating the pressure changes over time.  The chosen interval is known as the sampling rate. The sampling rate must be chosen correctly, or we cannot represent specific frequencies or introduce unintended distortions.`,
-    answer: `THOUGHT: The answer must complete the partial sentence. The Ttext is about characteristics of waves such as amplitude, frequency, and phase.
+Typical sound waves are complex and consist of multiple waves, each with amplitude, frequency, and phase.
+When we digitize a sound, we sample the amplitude, the difference compared to the base level, at fixed intervals. This gives a sequence of digital values that can be used to approximate the original sound wave by recreating the pressure changes over time. The <mask/> is known as the sampling rate. The sampling rate must be chosen correctly, or we cannot represent specific frequencies or introduce unintended distortions.`,
+    answer: `THOUGHT: The text is about digitizing sound waves and the needed properties. The <mask/> is an incomplete sentence starting with 'The' and ending with 'is known as the sampling rate.', I should avoid overlap with this. The answer is a description for the property known as the 'sampling rate'.
 LANGUAGE: English
-ANSWER: amplitude, frequency, and phase.`,
+ANSWER: chosen interval`,
 };
 
 export default example;
